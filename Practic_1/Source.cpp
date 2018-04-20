6caf24994413d5d1dec2ca6f7f7fb60b8a97e2fc
AleshkaPanov <thedefrostplayer@gmail.com>
2018-04-20 09:43:16 +0300

    first commit

---
+++ Practic_1/Source.cpp
@@ -0,0 +1,31 @@
+#include <cstring>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Dog
+{
+public:
+	Dog(string Nick, string Breed, float Age);
+	const string GetNick()  const { return Nick; }
+	const string GetBreed()  const { return Breed; }
+	const float GetAge() const { return Age; }
+private:
+	float Age;
+	string Breed;
+	string Nick;
+};
+
+Dog::Dog(string Nick, string Breed, float Age)
+{
+	this->Nick = Nick;
+	this->Breed = Breed;
+	this->Age = Age;
+}
+
+int main()
+{
+	Dog Ob("Bobick", "Pug", 2);
+	std::cout << Ob.GetNick() << ' ' << Ob.GetBreed() << ' ' << Ob.GetAge() << '\n';
+	return 0;
+}
